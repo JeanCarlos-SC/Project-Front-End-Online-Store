efeffc4513d9e5eb14c1195024a6683b95e15f84
Jean Carlos <jncorreia44@gmail.com>
2022-10-01 12:12:08 -0300

    FEAT: Adiciona projeto ao repositório

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Carrinho from './pages/Carrinho';
+import Product from './pages/Product';
+import Checkout from './pages/Checkout';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/carrinho" component={ Carrinho } />
+          <Route exact path="/product/:id" component={ Product } />
+          <Route exact path="/checkout" component={ Checkout } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
